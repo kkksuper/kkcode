2f05e1d48a8be4e80f0b11faaa5f5a0c8445c272
hzli <947493702@qq.com>
2023-10-23 21:07:49 +0800

    version 0.0.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='kkcode',
-    version='0.0.4',
+    version='0.0.5',
     packages=['kkTools'],
     url='https://gitee.com/kksuperr/kkcode',
     license='MIT',
