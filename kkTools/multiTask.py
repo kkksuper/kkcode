cd844a2255e71b45ce42f9a0001fcb65c476d063
hzli <947493702@qq.com>
2023-11-06 11:47:31 +0800

    add tqdm and dicitem in multitask when use cuda

--- kkTools/multiTask.py
+++ kkTools/multiTask.py
@@ -28,7 +28,7 @@ def multiThread_use_multiprocessing_multiarg(scp, numthread, func, *args):
         tmp.start()
 
 
-def multiThread_use_multiprocessing_dicarg_spawn(scp, numthread, func, args):
+def multiThread_use_multiprocessing_dicarg_spawn(scp, numthread, func, args, use_tqdm=True):
     '''
     根据scp得到要处理的文件名单, 创建numthread个线程, 调用func函数, 并传入若干参数以及该线程要处理的scp \n
     共享显卡 \n
@@ -42,15 +42,47 @@ def multiThread_use_multiprocessing_dicarg_spawn(scp, numthread, func, args):
     ctx = torch.multiprocessing.get_context("spawn")
     pool = ctx.Pool(numthread)
     pool_list = []
+    
+    tqdmbar= tqdm(total=len(scp))
+    update = lambda *tmp: tqdmbar.update()
 
     for item in scp:
-        res = pool.apply_async(partial(func, item, **args))
+        res = pool.apply_async(partial(func, item, **args), callback=update if use_tqdm else None)
         pool_list.append(res)
     
     pool.close()  # 关闭进程池，不再接受新的进程
     pool.join()  # 主进程阻塞等待子进程的退出
 
-    return [i.get() for i in tqdm(pool_list)]
+    return [i.get() for i in pool_list]
+
+
+def multiThread_use_multiprocessing_dicitem_dicarg_spawn(scp, numthread, func, args, use_tqdm=True):
+    '''
+    根据scp得到要处理的名单(字典数组), 创建numthread个线程, 调用func函数, 并传入若干参数以及该线程要处理的scp \n
+    共享显卡 \n
+    Parameters: \n
+        scp - 要处理的文件名list \n
+        numthread - 线程数 \n
+        func - 调用的函数, 该函数会接收到两部分参数, 一个文件名和args \n
+        args - 传入的一个字典参数, 执行函数最终会收到多个参数 \n
+    '''
+    import torch.multiprocessing
+    ctx = torch.multiprocessing.get_context("spawn")
+    pool = ctx.Pool(numthread)
+    pool_list = []
+    
+    if use_tqdm:
+        tqdmbar= tqdm(total=len(scp))
+        update = lambda *tmp: tqdmbar.update()
+
+    for item in scp:
+        res = pool.apply_async(partial(func, **item, **args), callback=update if use_tqdm else None)
+        pool_list.append(res)
+    
+    pool.close()  # 关闭进程池，不再接受新的进程
+    pool.join()  # 主进程阻塞等待子进程的退出
+
+    return [i.get() for i in pool_list]
 
 
 def multiThread_use_ProcessPoolExecutor_dicarg(scp, numthread, func, args, use_tqdm=True, use_valid=False):
