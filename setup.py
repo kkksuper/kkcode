a083856e01a524f8401556eb823d7838e69ba170
hanzhaoli <hanzhaoli@tencent.com>
2024-10-28 16:29:23 +0800

    change version to 0.0.8

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='kkcode',
-    version='0.0.7',
+    version='0.0.8',
     packages=['kkTools'],
     url='https://gitee.com/kksuperr/kkcode',
     license='MIT',
